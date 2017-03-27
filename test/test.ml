e4fd70f021012349e1e7c7226810cfb507349e4f
Daniel Bünzli <daniel.buenzli@erratique.ch>
2017-03-27 13:51:56 +0200

    Add more tests.

--- test/test.ml
+++ test/test.ml
@@ -58,10 +58,29 @@ let listo d dl =
   let tl l = Rel.(pure List.tl |> app dl l |> ret dl) in
   let list l = List.fold_right (fun x xs -> cons (Rel.const d x) xs) l empty in
   let conso x xs l = Rel.(cons x xs = l) in
-  empty, cons, hd, tl, list, conso
+  let heado l x = Rel.(fresh @@ fun xs -> cons x xs = l) in
+  let tailo l xs = Rel.(fresh @@ fun x -> cons x xs = l) in
+  let rec appendo l0 l1 l =
+    (* List.append [] l = l ||
+       List.append (x :: xs) l = x :: (List.append xs l) *)
+    let open Rel in
+    (l0 = empty && l1 = l) ||
+    (fresh @@ fun x -> fresh @@ fun xs -> fresh @@ fun tl ->
+     (cons x xs) = l0 &&
+     (cons x tl) = l  &&
+     delay @@ lazy (appendo xs l1 tl))
+  in
+  let rec revo l r =
+    let open Rel in
+    (l = empty && r = empty) ||
+    (fresh @@ fun x -> fresh @@ fun xs -> fresh @@ fun rt ->
+     (cons x xs) = l &&
+     appendo rt (cons x empty) r &&
+     delay @@ lazy (revo xs rt))
+  in
+  empty, cons, hd, tl, list, conso, heado, tailo, appendo, revo
 
-
-let iempty, icons, ihd, itl, ilist, iconso =
+let iempty, icons, ihd, itl, ilist, iconso, iheado, tailo, iappendo, irevo =
   listo Rel.Dom.int Rel.Dom.(list int)
 
 let il = ilist [1;2;3]
@@ -82,47 +101,36 @@ let m4tch x xs = Rel.(iconso x xs (ilist [1;2;3]))
 let m4tch = Rel.(var @@ var @@ reify m4tch (fun x xs -> x, xs))
 let () = assert (Rel.(all @@ get m4tch) = [(1, [2;3])])
 
-let rec appendo l0 l1 l =
-  (* List.append [] l = l ||
-     List.append (x :: xs) l = x :: (List.append xs l) *)
-  Rel.((l0 = iempty && l1 = l) ||
-       (fresh @@ fun x ->
-        fresh @@ fun xs ->
-        fresh @@ fun l' ->
-        iconso x xs l0 &&
-        iconso x l' l &&
-        delay @@ lazy (appendo xs l1 l')))
-
-let l l = appendo (ilist [1;2;3]) (ilist [4;5;6]) l
+let l l = iappendo (ilist [1;2;3]) (ilist [4;5;6]) l
 let l = Rel.(var @@ reify l id)
 let () = assert (Rel.(all @@ get l) = [[1;2;3;4;5;6]])
 
-let l l = appendo l (ilist [4;5;6]) (ilist [1;2;3;4;5;6])
+let l l = iappendo l (ilist [4;5;6]) (ilist [1;2;3;4;5;6])
 let l = Rel.(var @@ reify l id)
 let () = assert (Rel.(all @@ get l) = [[1;2;3]])
 
-let l l = appendo (ilist [1;2]) l (ilist [1;2;3;4;5;6])
+let l l = iappendo (ilist [1;2]) l (ilist [1;2;3;4;5;6])
 let l = Rel.(var @@ reify l id)
 let () = assert (Rel.(all @@ get l) = [[3;4;5;6]])
 
-let l = appendo (ilist [1]) (ilist [2]) (ilist [3])
+let l = iappendo (ilist [1]) (ilist [2]) (ilist [3])
 let () = assert (not (Rel.success l))
 
 let l pre =
   Rel.(fresh @@ fun suf ->
-       appendo pre suf (ilist [1;2;3;4]))
+       iappendo pre suf (ilist [1;2;3;4]))
 
 let l = Rel.(var @@ reify l id)
 let () = assert (Rel.(all @@ get l) = [[]; [1]; [1;2]; [1;2;3]; [1;2;3;4]])
 
 let l suf =
   Rel.(fresh @@ fun pre ->
-       appendo pre suf (ilist [1;2;3;4]))
+       iappendo pre suf (ilist [1;2;3;4]))
 
 let l = Rel.(var @@ reify l id)
 let () = assert (Rel.(all @@ get l) = [[1;2;3;4]; [2;3;4]; [3;4]; [4]; []])
 
-let l pre suf = appendo pre suf (ilist [1;2;3;4])
+let l pre suf = iappendo pre suf (ilist [1;2;3;4])
 let l = Rel.(var @@ var @@ reify l (fun p s -> (p, s)))
 let () = assert (Rel.(all @@ get l) =
                  [([], [1;2;3;4]);
@@ -130,6 +138,45 @@ let () = assert (Rel.(all @@ get l) =
                   ([1;2], [3;4]);
                   ([1;2;3], [4]);
                   ([1;2;3;4], [])])
+
+let r l = irevo (ilist [1;2;3]) l
+let r = Rel.(var @@ reify r id)
+let () = assert (Rel.(head @@ get r) = [3;2;1])
+
+module Btree = struct
+  type t = Node of int * t * t | Nil
+  let rec equal t0 t1 = match t0, t1 with
+  | Nil, Nil -> true
+  | Node (v0,l0,r0), Node (v1,l1,r1) -> v0 = v1 && equal l0 l1 && equal r0 r1
+  | _, _ -> false
+
+  let rec pp ppf = function
+  | Nil -> Format.fprintf ppf "Nil"
+  | Node (v, l, r) ->
+      Format.fprintf ppf "@[Node @[<1>(%d,@ %a,@ %a)@]" v pp l pp r
+
+  let nil = Nil
+  let node v l r = Node (v, l, r)
+
+  let rec fold t f z =
+    let rec loop acc = function
+    | Nil -> z
+    | Node (v, l, r) -> f v
+    in
+    loop z t
+end
+
+let dom_btree = Rel.Dom.of_type (module Btree)
+
+let lnil = Rel.const dom_btree Btree.nil
+let lnode v l r = Rel.(pure Btree.node |>
+                       app Dom.int v |> app dom_btree l |> app dom_btree r |>
+                       ret dom_btree)
+
+let rec linj = function
+| Btree.Nil -> lnil
+| Btree.Node (v, l, r) -> lnode (Rel.int v) (linj l) (linj r)
+
 
 let () = print_endline "All tests succeeded!"
 
