7a761a1449405b511dff0f29592d61c46f1d09fc
Daniel Bünzli <daniel.buenzli@erratique.ch>
2017-03-30 10:38:02 +0200

    Add toplevel support.

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -6,6 +6,8 @@ open Topkg
 let () =
   Pkg.describe "rel" @@ fun c ->
   Ok [ Pkg.mllib "src/rel.mllib";
+       Pkg.mllib ~api:[] "src/rel_top.mllib";
+       Pkg.lib "src/rel_top_init.ml";
        Pkg.test "test/test";
        Pkg.test "test/test_tree";
        Pkg.test "test/test_list";
